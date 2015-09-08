701b17d59cfbaf19c8167fe8a1dcf3e1aa308840
Erik Butcher <with_a_k@mac.com>
2015-09-08 14:46:31 -0600

    Create layout and install Omniauth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,18 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :current_user, :user_information
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def authorize!
+    redirect_to root_path unless current_user
+  end
+
+  def user_information
+
+  end
 end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+  def create
+    auth = request.env['omniauth.auth']
+    user = User.find_or_create_from_oauth(auth)
+
+    if user
+      session[:user_id] = user.id
+      redirect_to feed_path
+    else
+      flash[:critical] = "Action failed!"
+      redirect_to landing_path
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to landing_path
+  end
+end
